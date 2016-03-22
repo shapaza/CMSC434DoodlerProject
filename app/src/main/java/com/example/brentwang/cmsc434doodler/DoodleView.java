1a03b7b94845b0d062cc31695d168811d3518f26
shapaza <wang_brent@yahoo.com>
2016-03-22 18:49:05 -0400

    added eraser, opacity, and clear options

--- app/src/main/java/com/example/brentwang/cmsc434doodler/DoodleView.java
+++ app/src/main/java/com/example/brentwang/cmsc434doodler/DoodleView.java
@@ -1,6 +1,7 @@
 package com.example.brentwang.cmsc434doodler;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -17,10 +18,16 @@ import android.util.TypedValue;
  */
 public class DoodleView extends View {
 
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
     private Paint _paintDoodle = new Paint();
+    private Paint canvasPaint;
     private Path _path = new Path();
+
     private int paintColor;
+    private int opacity = 255;
     private float brushSize, lastBrushSize;
+    private boolean erase=false;
 
     public DoodleView(Context context) {
         super(context);
@@ -32,7 +39,7 @@ public class DoodleView extends View {
         init(attrs, 0);
     }
 
-    public DoodleView(Context context,  AttributeSet attrs, int defStyle) {
+    public DoodleView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(attrs, defStyle);
     }
@@ -45,6 +52,43 @@ public class DoodleView extends View {
         _paintDoodle.setAntiAlias(true);
         _paintDoodle.setStyle(Paint.Style.STROKE);
         _paintDoodle.setStrokeWidth(brushSize);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    public int getOpacity(){
+        return Math.round((float)opacity/255*100);
+    }
+
+    public void setOpacity(int newOpacity){
+        opacity=Math.round((float)newOpacity/100*255);
+        _paintDoodle.setColor(paintColor);
+        _paintDoodle.setAlpha(opacity);
+    }
+
+    public void clear(){
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+
+    public void setErase(boolean isErase){
+        //set erase true or false
+        erase=isErase;
+
+        if(erase) {
+            _paintDoodle.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        }
+        else {
+            _paintDoodle.setXfermode(null);
+        }
     }
 
     public void setBrushSize(float newSize){
@@ -74,6 +118,8 @@ public class DoodleView extends View {
         super.onDraw(canvas);
 
         //canvas.drawLine(0, 0, getWidth(), getHeight(), _paintDoodle);
+        //canvas.drawPath(_path, _paintDoodle);
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
         canvas.drawPath(_path, _paintDoodle);
     }
 
@@ -90,6 +136,8 @@ public class DoodleView extends View {
                 _path.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(_path, _paintDoodle);
+                _path.reset();
                 break;
         }
 
--- app/src/main/java/com/example/brentwang/cmsc434doodler/MainActivity.java
+++ app/src/main/java/com/example/brentwang/cmsc434doodler/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 import java.util.UUID;
 import android.provider.MediaStore;
@@ -18,7 +21,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     public DoodleView doodleView;
     private ImageButton currPaint;
-    private Button drawBtn;
+    private Button drawBtn, opacityBtn, eraseBtn, clearBtn;
     private float smallBrush, mediumBrush, largeBrush;
 
     @Override
@@ -35,15 +38,28 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         mediumBrush = getResources().getInteger(R.integer.medium_size);
         largeBrush = getResources().getInteger(R.integer.large_size);
 
+        // the brush button, which allows users to change the size of the brush
         drawBtn = (Button)findViewById(R.id.draw_btn);
         drawBtn.setOnClickListener(this);
+
+        // opacity button
+        opacityBtn = (Button)findViewById(R.id.opacity_btn);
+        opacityBtn.setOnClickListener(this);
+
+        // erase button
+        eraseBtn = (Button)findViewById(R.id.erase_btn);
+        eraseBtn.setOnClickListener(this);
+
+        // clear button
+        clearBtn = (Button)findViewById(R.id.clear_btn);
+        clearBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view){
         //respond to clicks
 
-        // if the draw button is clicked
+        // if the brush button is clicked
         if(view.getId()==R.id.draw_btn){
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Brush size:");
@@ -53,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    doodleView.setErase(false);
                     doodleView.setBrushSize(smallBrush);
                     doodleView.setLastBrushSize(smallBrush);
                     brushDialog.dismiss();
@@ -63,6 +80,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    doodleView.setErase(false);
                     doodleView.setBrushSize(mediumBrush);
                     doodleView.setLastBrushSize(mediumBrush);
                     brushDialog.dismiss();
@@ -73,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             largeBtn.setOnClickListener(new OnClickListener(){
                 @Override
                 public void onClick(View v) {
+                    doodleView.setErase(false);
                     doodleView.setBrushSize(largeBrush);
                     doodleView.setLastBrushSize(largeBrush);
                     brushDialog.dismiss();
@@ -81,10 +100,99 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
             brushDialog.show();
         }
+
+        // if the opacity button is clicked
+        if(view.getId()==R.id.opacity_btn){
+            //launch opacity chooser
+
+            final Dialog seekDialog = new Dialog(this);
+            seekDialog.setTitle("Opacity level:");
+            seekDialog.setContentView(R.layout.opacity_chooser);
+            final TextView seekTxt = (TextView)seekDialog.findViewById(R.id.opq_txt);
+            final SeekBar seekOpq = (SeekBar)seekDialog.findViewById(R.id.opacity_seek);
+            seekOpq.setMax(100);
+
+            int currLevel = doodleView.getOpacity();
+            seekTxt.setText(currLevel+"%");
+            seekOpq.setProgress(currLevel);
+
+            seekOpq.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    seekTxt.setText(Integer.toString(progress) + "%");
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            });
+
+            Button opqBtn = (Button)seekDialog.findViewById(R.id.opq_ok);
+
+            opqBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.setOpacity(seekOpq.getProgress());
+                    seekDialog.dismiss();
+                }
+            });
+
+            seekDialog.show();
+        }
+
+        // if the erase button is clicked
+        if (view.getId()==R.id.erase_btn){
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Eraser size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.setErase(true);
+                    doodleView.setBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    doodleView.setErase(true);
+                    doodleView.setBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    doodleView.setErase(true);
+                    doodleView.setBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+
+        // if user presses clear
+        if(view.getId()==R.id.clear_btn) {
+            doodleView.clear();
+        }
     }
 
     public void paintClicked(View view){
-        //use chosen color
+        // stop erasing
+        doodleView.setErase(false);
+
+        // get the last brush size the user selected
+        doodleView.setBrushSize(doodleView.getLastBrushSize());
 
         if(view!=currPaint){
             //update color
